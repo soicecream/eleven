1302fa920e7e2d988b5a408155508ca244734384
asd <M2657040829@163.com>
2022-08-25 18:11:58 +0800

    okok

--- src/App.vue
+++ src/App.vue
@@ -30,32 +30,27 @@ export default {
     }
   },
 
-  // 创建阶段：数据的准备
-  created() {
-    this.$store.state.all_img_text = this.get_text_data('../../txt/BackgroundImg.txt')
-    this.$store.state.Journal_text = this.get_text_data('../../txt/Journal.txt')
-    this.$store.state.tongue_twister_text = this.get_text_data('../../txt/TongueTwister.txt')
-
-    window.addEventListener('scroll', this.check_roll_show)
+  // 计算属性
+  computed: {
 
   },
 
-  // 定义函数
+  // 定义函数  事件处理器
   methods: {
     // 获取文本数据
     get_text_data(file_url){
       //  更新数据market_id.txt文件接口
-      let xhr = new XMLHttpRequest(),
-          okStatus = document.location.protocol === "file:" ? 0 : 200;
-      xhr.open("GET", file_url, false); // 文件路径
-      xhr.overrideMimeType("text/html;charset=utf-8"); //默认为utf-8
-      xhr.send(null);
+      let xhr = new XMLHttpRequest()
+      let okStatus = document.location.protocol === "file:" ? 0 : 200
+      xhr.open("GET", file_url, false) // 文件路径
+      xhr.overrideMimeType("text/html;charset=utf-8") //默认为utf-8
+      xhr.send(null)
       // console.log(xhr.responseText) //文本的内容
-      return xhr.status === okStatus ? xhr.responseText : null;
+      return xhr.status === okStatus ? xhr.responseText : null
     },
 
     // 返回顶部
-    go_to_top() {
+    go_to_top () {
       // document.body.scrollTop = 0;
       // document.documentElement.scrollTop = 0;
 
@@ -68,7 +63,7 @@ export default {
       }, 16)
     },
     // 返回顶部是否显示
-    check_roll_show() {
+    check_roll_show () {
       let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
       this.scrollTop = scrollTop
       if (this.scrollTop > 0)
@@ -79,15 +74,71 @@ export default {
 
   },
 
-  // 页面离开时销毁侦听时间
+  // 侦听器
+  watch: {
+
+  },
+
+
+  // 下面都是 生命周期钩子
+
+  // 在实例初始化之后,进行数据侦听和事件/侦听器的配置之前同步调用。
+  beforeCreate() {
+
+  },
+
+  // 在实例创建完成后被立即同步调用。  创建阶段：数据的准备
+  created() {
+    this.$store.state.all_img_text = this.get_text_data('../../txt/BackgroundImg.txt')
+    this.$store.state.Journal_text = this.get_text_data('../../txt/Journal.txt')
+    this.$store.state.tongue_twister_text = this.get_text_data('../../txt/TongueTwister.txt')
+
+    window.addEventListener('scroll', this.check_roll_show)
+
+  },
+
+  // 实例被挂载后调用  挂载阶段：数据加载到页面
+  mounted () {
+
+  },
+
+  // 在数据发生改变后，DOM 被更新之前被调用。
+  beforeUpdate () {
+
+  },
+
+  // 在数据更改导致的虚拟 DOM 重新渲染和更新完毕之后被调用。   数据更新后调用
+  // 该钩子在服务器端渲染期间不被调用。
+  updated () {
+
+  },
+
+  // 被 keep-alive 缓存的组件激活时调用。
+  // 该钩子在服务器端渲染期间不被调用。
+  activated () {
+
+  },
+
+  // 被 keep-alive 缓存的组件失活时调用。
+  // 该钩子在服务器端渲染期间不被调用。
+  deactivated () {
+
+  },
+
+  // 实例销毁之前调用。在这一步，实例仍然完全可用。
+  // 该钩子在服务器端渲染期间不被调用。
+  beforeDestroy () {
+
+  },
+
+  // 实例销毁后调用。
+  //  该钩子被调用后，对应Vue实例的所有指令都被解绑，所有的事件监听器被移除，所有的子实例也都被销毁。
+  // 该钩子在服务器端渲染期间不被调用。
   destroyed () {
     window.removeEventListener('scroll', this.check_roll_show)
 
   },
-  // 也差不多
-  beforeDestroy () {
 
-  },
 
 }
 
